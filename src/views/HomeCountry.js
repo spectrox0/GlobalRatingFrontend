31b3a3b0c2a6d6086ad03794885492a2a7aa9b6f
spectrox0 <alejanvelazco2008@hotmail.com>
2019-08-29 20:59:50 -0400

    optimizacion

--- src/views/HomeCountry.js
+++ src/views/HomeCountry.js
@@ -1,6 +1,5 @@
 import React , {useState,useEffect } from 'react'; 
 import axios from 'axios'
-import $ from "jquery"; 
 
 export default function HomeCountry() {
 const [countryName, setCountryName] = useState(null); 
