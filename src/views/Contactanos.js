cdd04f19807ec376df060d4b1a9dda9b87055698
spectrox0 <alejanvelazco2008@hotmail.com>
2019-09-16 10:25:13 -0400

    cambios contactanos

--- src/views/Contactanos.js
+++ src/views/Contactanos.js
@@ -11,7 +11,8 @@ import {
   InputGroupText,
   Label,
   FormGroup,
-  Container
+  Container,
+  Button
 } from 'reactstrap'
 
 export default function () {
@@ -30,16 +31,6 @@ export default function () {
        };   });
  
 */
-  const DictamenContent = styled.div`
-   .main {
-       display: flex; 
-       flex-direction: column; 
-       align-items:center; 
-       justify-content:center; 
-       height:100vh; 
-      
-   }
-`
   return (
 
     <DictamenContent>
@@ -49,80 +40,110 @@ export default function () {
           <Container >
             <Row>
               <Col>
-                <h3>Nuestras Coordenadas </h3>
-
+              <Row> 
+              <h3>Nuestras Coordenadas </h3>
+              </Row>
+              
+              <Row> 
                 <div className="elementCoordenadas" >
                   <span>
                     Av. Ppal. de la Castellana, Centro Letonia, Piso 12, Oficina 126.
                     Caracas - Venezuela
                  </span>
+                 
                 </div>
+                </Row>
+                <Row> 
                 <div className="elementCoordenadas" >
                   <span>
                     Master +58 (212) 263.4393
                  </span>
                 </div>
+                </Row>
+                <Row> 
                 <div className="elementCoordenadas" >
                   <span>
                     info@globalratings.com
                  </span>
                 </div>
+                </Row>
               </Col>
               <Col>
                 <Form>
                   <Row>
                     <Col>
-                      <Label for="nombre" > Nombre</Label>
-                      <InputGroup>
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            {/* <i className="now-ui-icons ui-1_email-85"></i> */}
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input id="nombre" placeholder="Escribe tu nombre" type="text"></Input>
-                      </InputGroup>
+                    <FormGroup>
+                      <Label htmlFor="nombre" > Nombre</Label>
+                        <Input
+                         id="nombre" 
+                         placeholder="Escribe tu nombre" 
+                         type="text"
+                         className="textInput"
+                         ></Input>
+                        </FormGroup>
                     </Col>
                     <Col>
-                      <Label for="apellido" > Apellido</Label>
-                      <InputGroup>
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            {/* <i className="now-ui-icons ui-1_email-85"></i> */}
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input id="apellido" placeholder="Escribe tu apellido" type="text"></Input>
-                      </InputGroup></Col>
+                    <FormGroup>
+                      <Label htmlFor="apellido" > Nombre</Label>
+                        <Input 
+                        id="apellido" 
+                        placeholder="Escribe tu apellido" 
+                        type="text"
+                        className="textInput"
+                        ></Input>
+                        </FormGroup></Col>
                   </Row>
                   <Row>
                     <Col>
+                    <FormGroup> 
+                    <Label htmlFor="email" > Email </Label>
                       <InputGroup>
                         <InputGroupAddon addonType="prepend">
                           <InputGroupText>
                             <i className="now-ui-icons ui-1_email-85"></i>
                           </InputGroupText>
                         </InputGroupAddon>
-                        <Input placeholder="Escribe tu correo" type="text"></Input>
-                      </InputGroup></Col>
+                        <Input 
+                        id="email"
+                         placeholder="Escribe tu correo" 
+                         type="text"
+                         className="textInput"
+                        ></Input>
+                      </InputGroup>
+                      </FormGroup>
+                      </Col>
                   </Row>
                   <Row>
                     <Col>
-                      <InputGroup>
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="now-ui-icons ui-1_email-85"></i>
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input placeholder="Escribe tu correo" type="text"></Input>
-                      </InputGroup></Col>
+                    <FormGroup>
+                      <Label htmlFor="asunto" > Asunto </Label>
+                        <Input 
+                        id="asunto" 
+                        placeholder="Escribe tu asunto"
+                         type="text"
+                         className="textInput"
+                         ></Input>
+                        </FormGroup>
+                      </Col>
+
                   </Row>
                   <Row>
                     <Col>
                       <FormGroup>
-                        <Label for="textArea" > Mensaje</Label>
-                        <Input id="textArea" placeholder="Escribe tu mensaje" type="textarea"></Input>
+                        <Label htmlFor="textArea" > Mensaje</Label>
+                        <Input 
+                        id="textArea" 
+                        placeholder="Escribe tu mensaje" 
+                        type="textarea"
+                        className="textAreaMensaje"
+                        rows="3"
+                        ></Input>
                       </FormGroup>
                     </Col>
                   </Row>
+                  <Button className="btn-round" size="lg" color="info" type="submit">
+                   ENVIAR MENSAJE 
+                 </Button>
                 </Form>
               </Col>
             </Row>
@@ -134,5 +155,36 @@ export default function () {
 
 
 }
+
+const DictamenContent = styled.div`
+.main {
+    display: flex; 
+    flex-direction: column; 
+    align-items:center; 
+    justify-content:center; 
+    height:100vh; 
+   
+} 
+
+h3{
+  color:#4169E1;
+  text-transform:uppercase; 
+}
+.elementCoordenadas{
+  span {
+    font-size:1.2rem; 
+  }
+  margin-bottom:1rem; 
+} 
+  .textAreaMensaje,.textInput{
+    &:focus {
+     border-color:#1E90FF; 
+    }
+    
+  }
+  label {
+    color:#191970;
+  }
+`
 
 
