c7db86869ceab5a7a7675d88247b26212a32aef5
spectrox0 <alejanvelazco2008@hotmail.com>
2019-10-22 09:26:26 -0400

    cambios

--- src/views/Home.js
+++ src/views/Home.js
@@ -10,24 +10,12 @@ import IndexHeader from "components/Headers/IndexHeader.js";
 import Dictamenes from "views/home-sections/DictamenesDeCalificacion.js"
 import Noticias from "views/home-sections/NoticiasFinanzas.js"
 
-import styled from 'styled-components'; 
-
 import {initGA} from './helpers/initGA.js';
-import {MDBContainer} from 'mdbreact'
+
 import axios from 'axios'; 
 export default function Index() {
   
-  React.useEffect(() => {
-    document.body.classList.add("index-page");
-    document.body.classList.add("sidebar-collapse");
-    document.documentElement.classList.remove("nav-open");
-    window.scrollTo(0, 0);
-    document.body.scrollTop = 0;
-    return function cleanup() {
-      document.body.classList.remove("index-page");
-      document.body.classList.remove("sidebar-collapse");
-    };
-  });
+
   React.useEffect( ()=> {
     initGA();
     getJson(); 
