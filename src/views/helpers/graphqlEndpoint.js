86a49937492d56d4cec3cbe70949d761ee268462
spectrox0 <alejanvelazco2008@hotmail.com>
2019-12-09 14:00:55 -0400

    asdas

--- src/views/helpers/graphqlEndpoint.js
+++ src/views/helpers/graphqlEndpoint.js
@@ -1,18 +1,18 @@
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
 
-const endpoint = 'https://api-global-ratings.herokuapp.com/graphql'
+const endpoint = "https://api-global-ratings.herokuapp.com/graphql";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: endpoint
-// uri: 'http://localhost:4000/graphql'
-})
+  //uri: endpoint
+  uri: "http://localhost:4000/graphql"
+});
 
 const client = new ApolloClient({
   cache,
   link
-})
+});
 
-export default client; 
+export default client;
