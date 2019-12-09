617236a7f0659cdb671478f9f611d4247b978231
spectrox0 <alejanvelazco2008@hotmail.com>
2019-12-09 15:04:23 -0400

    asdas

--- src/views/helpers/graphqlEndpoint.js
+++ src/views/helpers/graphqlEndpoint.js
@@ -7,7 +7,7 @@ const endpoint = "https://api-global-ratings.herokuapp.com/graphql";
 const cache = new InMemoryCache();
 const link = new HttpLink({
   //uri: endpoint
-  uri: "http://localhost:4000/graphql"
+  uri: "http://127.0.0.1:4000/graphql"
 });
 
 const client = new ApolloClient({
